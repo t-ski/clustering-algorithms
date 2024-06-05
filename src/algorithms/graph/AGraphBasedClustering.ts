4272ed94e90eeddd4c13480a2ad7fa67b4dbec90
t-ski <t.schiepanski@gmx.de>
2024-06-06 01:18:21 +0200

    Add static vectors to adjacency matrix helper method to graph based clustering class

--- src/algorithms/graph/AGraphBasedClustering.ts
+++ src/algorithms/graph/AGraphBasedClustering.ts
@@ -1,8 +1,18 @@
-import { TMatrix } from "../../types";
+import { TMatrix, TVector } from "../../types";
 import { AClustering } from "../AClustering";
 
 
 export abstract class AGraphBasedClustering extends AClustering<TMatrix, number[][]> {
+	public static vectorsToAdjacencyMatrix(data: TVector[]): TMatrix {
+		const adjacencyMatrix: TMatrix = [];
+		for(let i = 0; i < data.length; i++) {
+			for (let j = 0; j < data.length; j++) {
+				adjacencyMatrix[i][j] = AClustering.distance(data[i], data[j]);
+			}
+		}
+		return adjacencyMatrix;
+	}
+
 	constructor(adjacencyMatrix: TMatrix) {
 		super(adjacencyMatrix);	// data ↔ Adjacency matrix
 	}
